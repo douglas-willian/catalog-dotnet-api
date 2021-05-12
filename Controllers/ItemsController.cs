8640847f8b8e2b004cdf3c1f924435b5ac24a983
Douglas Willian <doug.william@hotmail.com>
2021-05-12 14:56:22 -0300

    add GetItem by id to ItemController

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using catalog_dotnet_api.Entities;
 using catalog_dotnet_api.Repositories;
@@ -17,10 +18,22 @@ namespace catalog_dotnet_api.Controllers
       repository = new InMemItemsRepository();
     }
 
+    [HttpGet]
     public IEnumerable<Item> GetItems()
     {
       var items = repository.GetItems();
       return items;
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<Item> GetItem(Guid id) // ActionResult allows it to return more than one type. "NotFound" is different type of "Item"
+    {
+      var item = repository.GetItem(id);
+      if (item is null)
+      {
+        return NotFound();
+      }
+      return item;
     }
   }
 }
